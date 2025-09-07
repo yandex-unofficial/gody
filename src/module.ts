93d84e549344eb89621be350c6e9ca60ba3de4c7
NickRisinger <boozy044@gmail.com>
2025-09-07 13:35:12 +0300

    feat(module): add initial stub for src/module.ts

---
+++ src/module.ts
@@ -0,0 +1,15 @@
+import { defineNuxtModule } from 'nuxt/kit';
+
+export default defineNuxtModule({
+  meta: {
+    name: 'nuxt-varioqub',
+    configKey: 'varioqub',
+    compatibility: {
+      nuxt: '^3.0.0',
+    },
+  },
+  defaults: {},
+  setup(options, nuxt) {
+    console.log('setup', options, nuxt);
+  },
+});
