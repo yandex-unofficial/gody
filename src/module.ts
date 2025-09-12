bfb7417981d2c93ab55359dae107e3fb1d6dbeb5
NickRisinger <boozy044@gmail.com>
2025-09-12 18:50:13 +0300

    feat(module): declare ModuleOptions and expose public config; add Nuxt 4 compat

--- src/module.ts
+++ src/module.ts
@@ -1,15 +1,47 @@
-import { defineNuxtModule } from '@nuxt/kit';
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit';
+import { defu } from 'defu';
 
-export default defineNuxtModule({
+export interface ModuleOptions {
+  clientId: number;
+  enabled: boolean;
+  scriptSrc: string;
+}
+
+export interface ModulePublicRuntimeConfig {
+  varioqub: ModuleOptions;
+}
+
+declare module '@nuxt/schema' {
+  interface PublicRuntimeConfig extends ModulePublicRuntimeConfig {}
+  interface NuxtConfig {
+    varioqub?: ModuleOptions;
+  }
+  interface NuxtOptions {
+    varioqub: ModuleOptions;
+  }
+}
+
+export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'nuxt-varioqub',
     configKey: 'varioqub',
     compatibility: {
-      nuxt: '^3.0.0',
+      nuxt: '^3.0.0 || ^4.0.0',
     },
   },
-  defaults: {},
+  defaults: {
+    enabled: true,
+    scriptSrc: 'https://abt.s3.yandex.net/expjs/latest/exp.js',
+  },
   setup(options, nuxt) {
-    console.log('setup', options, nuxt);
+    const resolver = createResolver(import.meta.url);
+
+    nuxt.options.runtimeConfig.public.varioqub = defu(
+      nuxt.options.runtimeConfig.public.varioqub,
+      options,
+    );
+
+    addPlugin({ src: resolver.resolve('./runtime/plugins/varioqub.server'), mode: 'server' });
+    addPlugin({ src: resolver.resolve('./runtime/plugins/varioqub.client'), mode: 'client' });
   },
 });
